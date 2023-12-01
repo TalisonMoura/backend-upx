8ade4660ea6cd8af1172e34cc3027044bb04b43c
Talison Moura <talisonmoura13@gmail.com>
2023-12-01 19:34:10 -0300

    bugfix: string de conexão

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -5,16 +5,16 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(__dirname + '/../config/config.json');
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, config)
+  : new Sequelize(config.database, config.username, config.password, config);
+
+sequelize.authenticate()
+.then(() => { console.log('Connection has been established successfully.'); })
+.catch(err => { console.error('Unable to connect to the database:', err); });
 
 fs
   .readdirSync(__dirname)
